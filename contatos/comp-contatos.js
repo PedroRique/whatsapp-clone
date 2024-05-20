6a8558aa571ccf76ce3aeb7caa0212f66eae7e27
Pedro Rique <prique@patientnow.com>
2024-05-20 14:41:28 -0400

    novo contatos

--- contatos/comp-contatos.js
+++ contatos/comp-contatos.js
@@ -1,45 +1,40 @@
 class MyContacts extends HTMLElement {
-    constructor() {
-        super();
-        this.shadow = this.attachShadow({mode: 'open'});
-    }
-    connectedCallback() {
-        this.render();
-    }
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+  }
+  connectedCallback() {
+    this.render();
+  }
 
-    render() {
-        this.shadow.innerHTML = `
-        <link rel="stylesheet" href="contatos.css">
+  render() {
+    this.shadow.innerHTML = `
+        <link rel="stylesheet" href="contatos/contatos.css">
         <aside>
             <h2>Chats</h2>
+
+            <div class="chat-container">
+
+            
+            <my-avatar class="avatar"></my-avatar>
+
+            <div class="contact-info">
+                <div class="contact-header">
+
+                    <h3>Robertin Rei Delas</h3>
+                    <span class="tempo">12:01</span>
+                </div>
+                
+
+                <p class="contact-message">Qlf, visão, cria.</p>
+
+            </div>
+
+            
+            </div>
         </aside>
-    
-    <div class="chat-container">
-        <img src="src/eu-sabo-muito.png">
-        <span class="tempo">12:01</span>
-        <h3>Robertin Rei Delas</h3>
-        <p>Qlf, visão, cria.</p>
-       
-    </div>
-    
-    <div class="chat-container">
-        <img src="src/sabar muito.jpg">
-        <span class="tempo">12:03</span>
-        <h3>Nicola Tesla</h3>
-        <p>Tu viu aquela fita lá daquele bagulho lá que...</p>
-        
-    </div>
-    
-    <div class="chat-container">
-        <img src="src/sem foto.png">
-        <span class="tempo">12:55</span>
-        <h3>Agiota</h3>
-        <p>3 MESES ESPERANDO O PAGAMENTO</p> 
-    </div>
         `;
-    }
-
-
+  }
 }
 
-customElements.define('my-contacts', MyContacts);
+customElements.define("my-contacts", MyContacts);
